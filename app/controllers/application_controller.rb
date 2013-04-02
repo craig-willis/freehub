5e522287b4e6fb4e12086a45972aea8d4770330f
cwillis <cawillis@gmail.com>
2013-04-01 21:22:45 -0500

    Added requires_staff_password filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,4 +46,13 @@ class ApplicationController < ActionController::Base
     Date.new params[:year].to_i, params[:month].to_i, params[:day].to_i
   end
 
+  def requires_staff_password
+    if (session[:staff_user].empty?)
+      redirect_to  new_staff_path
+    end
+  end
+
+  def store_location
+    session[:stored_location] = request.fullpath
+  end
 end
