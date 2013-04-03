37fddf1ffff6507175b332f381a05b006f70df2a
cwillis <cawillis@gmail.com>
2013-04-02 20:00:37 -0500

    Added tbp seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+namespace :db do
+  desc "Load seed fixtures (from tbp) into the current environment's database." 
+  task :seed => :environment do
+    require 'active_record/fixtures'
+    Dir.glob(RAILS_ROOT + '/tbp/*.yml').each do |file|
+      Fixtures.create_fixtures('tbp', File.basename(file, '.*'))
+    end
+  end
+end
