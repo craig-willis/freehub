9db4c26202c913b5a632c1888750b5d7471af05c
craig-willis <willis8@illinois.edu>
2013-06-27 18:11:38 -0500

    Improved membership card layout

--- app/models/membership_card.rb
+++ app/models/membership_card.rb
@@ -5,11 +5,18 @@ class MembershipCard < Prawn::Document
           :page_layout => :landscape,
           :margin => [15,15,15,15])
     logopath =  "#{Rails.root}/app/assets/images/tbp-logo.png"
-    image logopath, :width => 30
-    text "The Bike Project of Urbana-Champaign"
-    text "Name: #{person.full_name}"
-    text "Type: #{person.membership.service_type.name}"
-    text "Started: #{person.membership.start_date}"
-    text "Expires: #{person.membership.end_date}"
+    image logopath, :width => 100
+    move_down 15
+    text "<font size='10'>Name: #{person.full_name}\n" +
+             "Type: #{person.membership.service_type.name}\n" +
+             "Membership Expires: #{person.membership.end_date}</font>\n", :inline_format => true
+    move_down 20
+    stroke_horizontal_rule
+    move_down 5
+    text "<font size='5'>" +
+             "202 S. Broadway, Room 24, Urbana, Illinois 61801\n" +
+             "thebikeproject@gmail.com, (217) 469-5126\n" +
+             "www.thebikeproject.org" +
+             "</font>", :align => :center, :inline_format => true
   end
 end
